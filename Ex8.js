5296e6ded6123ebf7c39a0091f15db23c709b355
Henrique-R-Abduch <henriqueabduch.gmail.com>
2023-04-06 16:44:42 -0300

    Ex8

---
+++ Ex8.js
@@ -0,0 +1,8 @@
+// O operador % em JavaScript é conhecido como operador módulo ou resto da divisão. Ele retorna o resto da divisão entre dois números. Logo, em uma divisão de '11' por '3', o número que irá sobrar da divisão é 2, visto que, ao multiplicarmos o número '3' três vezes ele irá retornar o número '9' e ficará faltando '2'. Segue o Exemplo:
+
+const a = 11;
+const b = 3;
+
+const resto = a % b;
+
+console.log(resto);
